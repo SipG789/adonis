6b81b98b560ecbc23f752a8b47ae4aacfc5dc000
Sip <sipg789@gmail.com>
2022-04-13 19:07:24 -0500

    added todo statements to get started

---
+++ index.js
@@ -0,0 +1,20 @@
+//TODO: add package.json file 
+//TODO: class for team manager, employee id, email address
+
+//TODO: java file for employee
+//TODO: add class for role prompt to enter the team manager’s name, employee ID, email address, and office number
+
+//TODO: add constructor function add this.name after then add getName() after but within the class function 
+
+//TODO: present a menu with the option to add an engineer or an intern or to finish building my team
+
+//TODO: enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu for engineer
+
+//TODO: enter the intern’s name, ID, email, and school, and I am taken back to the menu for intern
+
+//TODO: add css style
+//TODO: dist=== rendered output (HTML) and CSS style sheet
+//TODO: lib=== classes
+//TODO: src=== template helper code 
+//TODO: index.js=== runs the application 
+//TODO: HTML is generated
